1703475e015483b55ad6157c0a5a20a558901f61
gabrieltitato <titatoou2@hotmail.com>
2024-11-05 14:05:57 -0300

    feat: arrumando login e home

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,6 +7,7 @@ import { redirect } from "next/navigation";
 
 const LoginPage = async () => {
   const { userId } = await auth();
+
   if (userId) {
     redirect("/");
   }
@@ -23,7 +24,7 @@ const LoginPage = async () => {
           className="mb-8"
         />
         <h1 className="mb-3 text-4xl font-bold">Bem-vindo</h1>
-        <p className="text-muted-foreground mb-8">
+        <p className="mb-8 text-muted-foreground">
           A Finance AI é uma plataforma de gestão financeira que utiliza IA para
           monitorar suas movimentações e oferecer insights personalizados,
           facilitando o controle do seu orçamento.
