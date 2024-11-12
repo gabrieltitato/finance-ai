91266a6c234dd3653a2e0476889ab1c71209fc45
gabrieltitato <titatoou2@hotmail.com>
2024-11-11 22:04:31 -0300

    feat: ensure transactions can be deleted

---
+++ app/transactions/_components/delete-transaction-button.tsx
@@ -0,0 +1,59 @@
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/app/_components/ui/alert-dialog";
+import { Button } from "@/app/_components/ui/button";
+import { TrashIcon } from "lucide-react";
+import { deleteTransaction } from "../_actions/delete-transaction";
+import { toast } from "sonner";
+
+interface DeleteTransactionButtonProps {
+  transactionId: string;
+}
+
+const DeleteTransactionButton = ({
+  transactionId,
+}: DeleteTransactionButtonProps) => {
+  const handleConfirmDeleteClick = async () => {
+    try {
+      await deleteTransaction({ transactionId });
+      toast.success("Transação deletada com sucesso!");
+    } catch (error) {
+      console.error(error);
+      toast.error("Erro ao deletar a transação!");
+    }
+  };
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger asChild>
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <TrashIcon />
+        </Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Deseja realmente deletar?</AlertDialogTitle>
+          <AlertDialogDescription>
+            Esta ação não pode ser desfeita. Isso vai remover permanentemente
+            essa transação.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          <AlertDialogAction onClick={handleConfirmDeleteClick}>
+            Continar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteTransactionButton;
